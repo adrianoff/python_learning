1de2f13a9aab187288955252b8cacc65ddb7d34c
adrianoff <adrianov87@gmail.com>
2019-09-20 17:03:47 +0300

    onelinked list

--- data_structure/linked_lists/one_linked_list.py
+++ data_structure/linked_lists/one_linked_list.py
@@ -3,3 +3,14 @@ class Node:
     def __init__(self, value=None, next_node=None):
         self.value = value
         self.next_node = next_node
+
+
+class OneLinkedList:
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.length = 0
+
+    def clear(self):
+        self.__init__()
+
