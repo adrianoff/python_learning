a26020e7cc654da1ac4f14ba0de900aadd4cd494
Pavel Adrianov <adrianov87@gmail.com>
2019-10-09 22:53:04 +0300

    insort

---
+++ fluent_python_book/02-array-seq/bisect_example.py
@@ -0,0 +1,46 @@
+import bisect
+import sys
+
+HAYSTACK = [1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30]
+NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
+
+ROW_FMT = '{0:2d} @ {1:2d}    {2}{0:<2d}'
+
+
+def demo(bisect_fn):
+    for needle in reversed(NEEDLES):
+        position = bisect_fn(HAYSTACK, needle)  # <1>
+        offset = position * '  |'  # <2>
+        print(ROW_FMT.format(needle, position, offset))  # <3>
+
+
+if __name__ == '__main__':
+
+    if sys.argv[-1] == 'left':    # <4>
+        bisect_fn = bisect.bisect_left
+    else:
+        bisect_fn = bisect.bisect
+
+    print('DEMO:', bisect_fn.__name__)  # <5>
+    print('haystack ->', ' '.join('%2d' % n for n in HAYSTACK))
+    demo(bisect_fn)
+
+    new_haystack = [4, 12, 1, 100, 0]
+    print(bisect.bisect(new_haystack, 7))
+    print(bisect.bisect(sorted(new_haystack), 7))
+
+
+    ##
+    ## insort
+    ##
+
+    l = []
+    bisect.insort(l, 1)
+    print(l)
+    bisect.insort(l, 0)
+    print(l)
+    bisect.insort(l, 11)
+    print(l)
+    bisect.insort(l, -1)
+    print(l)
+
--- fluent_python_book/02-array-seq/sort.py
+++ fluent_python_book/02-array-seq/sort.py
@@ -1,4 +1,16 @@
+from random import shuffle
+
 l = [3, 3, 5, 0, 2, 8, 19, -1]
 print(l.sort())
 print(l)
 
+t = (3, 111, 3, 5, 1, 0)
+print(type(sorted(t)))
+
+fruits = ['grape', 'raspberry', 'apple', 'banana']
+print(sorted(fruits))
+print(sorted(fruits, reverse=True))
+print(sorted(fruits, key=len))
+
+fruits.sort()
+print(fruits)
