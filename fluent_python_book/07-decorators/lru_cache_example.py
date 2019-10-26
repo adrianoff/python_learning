eec71afbf262d1531270c8405d9e99a71bb55430
Pavel Adrianov <adrianov87@gmail.com>
2019-10-26 18:01:49 +0300

    decorators

--- fluent_python_book/07-decorators/lru_cache_example.py
+++ fluent_python_book/07-decorators/lru_cache_example.py
@@ -1,10 +1,12 @@
 import functools
-from time_decorator import my_timeit
+from fluent_python_book.decorators7.time_decorator import my_timeit
 
+
+@my_timeit
 def fibonacci(n):
     if n < 2:
         return n
     return fibonacci(n-2) + fibonacci(n-1)
 
-@my_timeit
+
 print(fibonacci(30))
