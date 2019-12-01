b7c25eb2043f5a9272fa4a3f13eac2ffd4ee2cd7
Pavel Adrianov <adrianov87@gmail.com>
2019-12-01 20:14:53 +0300

    one_mistake_between_strings

--- algoritms/my_tranings.py
+++ algoritms/my_tranings.py
@@ -20,19 +20,56 @@
 
 def is_palindrom(string):
     len_string = len(string)
-    s1 = string[:len_string//2]
+    s1 = string[:len_string // 2]
     if len_string % 2 == 0:
-        s2 = string[(len_string//2):]
+        s2 = string[(len_string // 2):]
     else:
-        s2 = string[(len_string // 2)+1:]
+        s2 = string[(len_string // 2) + 1:]
 
     if s1 == s2[::-1]:
         return True
     else:
         return False
 
-#print(is_palindrom('ctabatc'))
+
+# print(is_palindrom('ctabatc'))
+
+def one_mistake_between_strings(s1: str, s2: str):
+    if s1 == s2:
+        return True
+
+    if abs(len(s1) - len(s2)) > 1:
+        return False
+
+    if len(s1) < len(s2):
+        s1, s2 = s2, s1
+
+    for i, c in enumerate(s1):
+        s = s1[:i] + s1[i+1:]
+        if s == s2:
+            return True
+
+    if len(s1) == len(s2):
+        for i, c in enumerate(s1):
+            for b in range(ord('a'), ord('z')+1):
+                character = chr(b)
+                s = list(s1)
+                s[i] = character
+                s = ''.join(s)
+
+                if s == s2:
+                    return True
+
+    return False
 
 
+def test_functions(func: callable):
+    print(func.__doc__)
+
+    arg1 = 'pale'
+    arg2 = 'bale'
+    result = func(arg1, arg2)
+    print('OK' if result is True else 'Fail')
 
 
+test_functions(one_mistake_between_strings)
