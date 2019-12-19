59bc73e15e2caa1653cedfec13b6d177316a38a5
Pavel Adrianov <adrianov87@gmail.com>
2019-12-20 00:53:16 +0300

    bin_tree

--- data_structure/bin_tree/bin_tree.py
+++ data_structure/bin_tree/bin_tree.py
@@ -11,3 +11,22 @@ class BinTree:
         self.root = root
 
 
+import json
+
+with open('tree.json') as fp:
+    tree_json = json.load(fp)
+
+node = Node(value=tree_json['root']['value'])
+
+
+def fill_tree(node, dict):
+    node.value = dict.get('value', None)
+    if dict.get('left') is not None:
+        node.left = fill_tree(Node(parent=node), dict.get('left'))
+    if dict.get('right') is not None:
+        node.right = fill_tree(Node(parent=node), dict.get('right'))
+
+    return node
+
+
+node = fill_tree(Node(), tree_json['root'])
