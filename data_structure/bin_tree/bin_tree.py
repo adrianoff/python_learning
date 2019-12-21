8f5a464fe1b9c3a00ed3f1e79eab185b9f100055
adrianoff <adrianov87@gmail.com>
2019-12-21 17:49:42 +0300

    bin_tree

--- data_structure/bin_tree/bin_tree.py
+++ data_structure/bin_tree/bin_tree.py
@@ -10,23 +10,38 @@ class BinTree:
     def __init__(self, root: Node):
         self.root = root
 
+    def fill_tree(self, node, dict):
+        node.value = dict.get('value', None)
+        if dict.get('left') is not None:
+            node.left = self.fill_tree(Node(parent=node), dict.get('left'))
+        if dict.get('right') is not None:
+            node.right = self.fill_tree(Node(parent=node), dict.get('right'))
+
+        return node
+
+    def tree_to_dict(self, node=None):
+        if node is None:
+            node = self.root
+
+        dict = {'value': node.value}
+
+        if isinstance(node.left, Node):
+            dict['left'] = self.tree_to_dict(node.left)
+        if isinstance(node.right, Node):
+            dict['right'] = self.tree_to_dict(node.right)
+
+        return dict
 
 import json
 
 with open('tree.json') as fp:
     tree_json = json.load(fp)
 
-node = Node(value=tree_json['root']['value'])
+root_node = Node(value=tree_json['root']['value'])
+tree = BinTree(root_node)
+tree.fill_tree(tree.root, tree_json['root'])
+
+dict = tree.tree_to_dict()
 
 
-def fill_tree(node, dict):
-    node.value = dict.get('value', None)
-    if dict.get('left') is not None:
-        node.left = fill_tree(Node(parent=node), dict.get('left'))
-    if dict.get('right') is not None:
-        node.right = fill_tree(Node(parent=node), dict.get('right'))
-
-    return node
-
-
-node = fill_tree(Node(), tree_json['root'])
+pass
