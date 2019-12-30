edbbaa750dbc571b92c65491b6bfc3d1cdee5caa
adrianoff <adrianov87@gmail.com>
2019-12-31 02:07:01 +0300

    hanoi

--- algoritms/hanoi_towers/hanoi_towers.py
+++ algoritms/hanoi_towers/hanoi_towers.py
@@ -4,20 +4,16 @@ Disk = namedtuple('Disk', 'size')
 
 
 class Stack:
-    _items = []
+    def __init__(self):
+        self._items = []
 
     def push(self, disk: Disk):
         self._items.append(disk)
 
     def pop(self):
         disk = self._items[-1]
-        del(self._items[-1])
+        del (self._items[-1])
         return disk
-
-    def get_last(self):
-        if len(self._items) > 0:
-            return None
-        return self._items[-1]
 
     def __len__(self):
         return len(self._items)
@@ -30,8 +26,16 @@ stack1.push(Disk(size=5))
 stack1.push(Disk(size=3))
 stack1.push(Disk(size=1))
 
-d = stack1.pop()
+stack2 = Stack()
+stack3 = Stack()
 
 
+def move(h: int, stack1: Stack, stack2: Stack, stack3: Stack):
+    if h >= 1:
+        move(h-1, stack1, stack3, stack2)
+        stack2.push(stack1.pop())
+        move(h-1, stack3, stack2, stack1)
 
 
+move(len(stack1), stack1, stack2, stack3)
+pass
