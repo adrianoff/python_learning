8d4e6b58be9d1f010a9918dd06d114214002d1dd
adrianoff <adrianov87@gmail.com>
2021-10-06 13:41:29 +0300

    algoritm training

--- algoritms/a3b3c3d3.py
+++ algoritms/a3b3c3d3.py
@@ -1,8 +1,14 @@
-n = 1000
+n = 100
 
 for a in range(0, n):
     for b in range(0, n):
         for c in range(0, n):
-            for d in range(0, n):
-                if a**3 + b ** 3 == c ** 3 + d ** 3:
-                    print(a, b, c, d)
+            h = a**3 + b ** 3 - c ** 3
+            if h < 0:
+                break
+            d = int(round(h ** (1/3)))
+            if d > n:
+                break
+            if a**3 + b ** 3 == c ** 3 + d ** 3 and a+b != c+d:
+                print(a, b, c, d)
+                break
